72471ebea54d36d6e266b4e4b46ae8cede7ef6a1
ernest-emmanuel-utibe <em123nest@gmail.com>
2023-11-05 04:49:36 +0100

    String stage of the project

---
+++ src/main/java/com/ecommerce/shopHive/ShopHiveApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/shopHive/ShopHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
